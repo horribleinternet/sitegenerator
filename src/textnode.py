7e8ee087e4769859066cf77460d648996b9bf06c
horribleinternet <cyberblah@gmail.com>
2025-06-11 21:21:26 +0900

    fix leaf node

--- src/textnode.py
+++ src/textnode.py
@@ -36,6 +36,6 @@ def text_node_to_html_node(text_node):
         case TextType.LINK:
             return LeafNode("a", text_node.text, {"href":text_node.url})
         case TextType.IMAGE:
-            return LeafNode("img", text_node.text, {"src":text_node.url, "alt":text_node.text})
+            return LeafNode("img", None, {"src":text_node.url, "alt":text_node.text})
         case _:
             raise ValueError("invalide TextType")
